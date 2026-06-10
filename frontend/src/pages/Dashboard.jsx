f8fa98afa6b6d87dc9d451eb1d3484d869a239b1
Devansh270 <mehtadevansh27@gmail.com>
2026-06-10 21:39:53 +0530

    added prediction panel in frontend

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,14 @@
 import MetricsPanel from '../components/MetricsPanel'
 import ProtocolBreakdown from '../components/ProtocolBreakdown'
+import PredictionsPanel from "../components/PredictionsPanel";
+
 
 export default function DashboardPage() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
       <MetricsPanel />
       <ProtocolBreakdown />
+      <PredictionsPanel />
     </div>
   )
 }
